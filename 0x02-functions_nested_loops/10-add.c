805e1fe11af631b6f3420b7e7940fc05df689489
leukeng <114169719+leukeng@users.noreply.github.com>
2022-10-20 16:16:29 -0700

    Add files via upload

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @num1: The first integer to the added.
+ * @num2: The second nteger to the added.
+ *
+ * Return: The result of the addition.
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
