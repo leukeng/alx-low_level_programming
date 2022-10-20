8358036efaa2de949dffd3ce06d22bf2448b452f
leukeng <114169719+leukeng@users.noreply.github.com>
2022-10-20 09:50:28 -0700

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -38,3 +38,17 @@ void print_to_98(int n);
 void print_times_table(int n);
 
 #endif
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+
+{
+	return (write(1, &c, 1));
+}
