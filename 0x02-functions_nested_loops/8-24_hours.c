b2dcc290242e50441e9d8a935ee703d0a9a6efc2
leukeng <ekengeffiong21@gmail.com>
2022-10-20 16:39:04 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,8 @@ void jack_bauer(void)
 	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
 	hours_max = 58;
-        hours_tens = '0';
+	hours_tens = '0';
+
 	while (hours_tens < '3')
 	{
 		if (hours_tens == '2')
@@ -32,7 +33,7 @@ void jack_bauer(void)
 					_putchar(minutes_ones);
 					_putchar('\n');
 					minutes_ones++;
-				}
+		}
 				minutes_ones = '0';
 				minutes_tens++;
 			}
