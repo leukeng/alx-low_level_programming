0b7021815a6e4d3b924755f4a86b0f506aaccada
leukeng <ekengeffiong21@gmail.com>
2022-10-20 10:03:13 -0700

    task 0 completed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *  * main - Entry point
+ *   * Description: putchar function
+ *    * Return: 0;
+ *     */
+int main(void)
+{
+		putchar('');
+			_putchar('p');
+				_putchar('u');
+					_puthcar('t');
+						_putchar('c');
+							_putchar('h');
+								_putchar('a');
+									_putchar('r');
+										_putchar('/n');
+
+											return (0);
+}
