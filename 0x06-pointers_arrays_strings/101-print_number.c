ba696cd978d2c57e632dd730858378f0816dfc67
leukeng <ekengeffiong21@gmail.com>
2022-10-27 16:20:55 -0700

    task 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "main.h"
+
+/**
+ * main - generates keygen.
+ * Return: 0 Always.
  * print_number - prints an integer.
  * @n: input integer.
  * Return: no return.
- * void print_number(int n)
-	* unsigned int m, d, count;
+ */
+int main(void)
+void print_number(int n)
+{
+	int r = 0, c = 0;
+	time_t t;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
@@ -23,3 +35,18 @@
 		d /= 10;
 		count *= 10;
 	}
+
+	srand((unsigned int) time(&t));
+	while (c < 2772)
+	for (; count >= 1; count /= 10)
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
+		_putchar(((m / count) % 10) + 48);
+	}
+	printf("%c\n", (2772 - c));
+	return (0);
+}
